09288a323fbb5a373e410364cf2741f407325af0
Sławek Piotrowski <slawomir.piotrowski@payu.pl>
2012-12-30 17:56:48 +0100

    Initial commit

---
+++ src/main/java/org/atteo/evo/urlhandlers/ClasspathUrlStreamHandler.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2012 Atteo.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.atteo.evo.urlhandlers;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLStreamHandler;
+
+@UrlHandler("classpath")
+public class ClasspathUrlStreamHandler extends URLStreamHandler {
+	@Override
+	protected URLConnection openConnection(URL url) throws IOException {
+		URL resourceUrl = Thread.currentThread().getContextClassLoader().getResource(url.getPath());
+
+		if (resourceUrl == null) {
+			resourceUrl = ClasspathUrlStreamHandler.class.getClassLoader().getResource(url.getPath());
+
+			if (resourceUrl == null) {
+				throw new FileNotFoundException("Resorce not found on classpath: " + url.getPath());
+			}
+		}
+
+		return resourceUrl.openConnection();
+	}
+}
---
+++ src/test/java/org/atteo/evo/urlhandlers/UrlHandlerTest.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2012 Atteo.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.atteo.evo.urlhandlers;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class UrlHandlerTest {
+	@Before
+	public void setup() {
+		UrlHandlers.registerAnnotatedHandlers();
+	}
+
+	@Test
+	public void simple() throws MalformedURLException, IOException {
+		String className = UrlHandlerTest.class.getCanonicalName().replace(".", "/") + ".class";
+		URL url = new URL("classpath:" + className);
+		URLConnection connection = url.openConnection();
+		connection.connect();
+	}
+
+}
